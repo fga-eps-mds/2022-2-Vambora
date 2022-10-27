d3ac45009ffb66ff8f75b6f4d0bef181b34e3ad0
SamuelRicardoDS <samuelricardods@gmail.com>
2022-10-27 10:32:57 -0300

    para guga

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="text-4xl font-bold">EU SOU LINDO</h1>
+      <h1 className="text-4xl font-bold">croware</h1>
     </div>
   );
 };
