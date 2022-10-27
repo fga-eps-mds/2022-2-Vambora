40c769626d56fcc1ea9a3c0f040c63b5f65363cd
GustavoHenrique23 <gustavohenriqueprivado@gmail.com>
2022-10-27 10:25:54 -0300

    Guga

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,9 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="text-4xl font-bold">
-        Boilerplate para o projeto de MDS do Grupo 1
-      </h1>
+      <h1 className="text-4xl font-bold">EU SOU LINDO</h1>
     </div>
   );
 };
