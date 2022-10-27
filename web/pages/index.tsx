9444289de902cec6ff8c75bf8dd3723734218979
Gustavo Kenzo Araki Takechi <gustavokenzo@Gustavos-MacBook-Pro.local>
2022-10-27 10:18:19 -0300

    add web

---
+++ web/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1 className="text-4xl font-bold">
+        Boilerplate para o projeto de MDS do Grupo 1
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
