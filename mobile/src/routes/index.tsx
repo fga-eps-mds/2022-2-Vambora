5f532a0009ebdc44b88607de7a7007c319f75f17
GustavoHenrique23 <gustavohenriqueprivado@gmail.com>
2022-11-29 20:22:48 -0300

    feat: atualization welcome page

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Welcome from '../pages/Welcome';
-import SingIn from '../pages/SingIn';
+// import Welcome from "../pages/Welcome";
+// import SingIn from "../pages/SingIn";
+import Loading from "../pages/Loading";
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
-    return (
-        <Stack.Navigator>
-
-            <Stack.Screen
-                name='Welcome'
-                component={Welcome}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name='SingIn'
-                component={SingIn}
-                options={{ headerShown: false }}
-            />
-
-        </Stack.Navigator>
-
-    );
-};
+  return (
+    <Stack.Navigator>
+      {/* <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SingIn"
+        component={SingIn}
+        options={{ headerShown: false }}
+      /> */}
+      <Stack.Screen
+        name="Loading"
+        component={Loading}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
