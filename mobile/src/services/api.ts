2e4336acb58f49c9bdce33467285c5a5bc0b3c1d
GustavoHenrique23 <gustavohenriqueprivado@gmail.com>
2022-12-15 09:41:01 -0300

    Fix: LocalHostPort Error Solved

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+export const localhost = "172.20.114.135";
+export const port = "3333";
+
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: `http://${localhost}:${port}`,
 });
