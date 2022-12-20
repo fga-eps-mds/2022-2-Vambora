22602e673d287259f3e17b9bab887278268f6b21
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2022-12-20 13:01:39 -0300

    feat: configure waypoints and search autocompletion

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -3,13 +3,26 @@ import {
   requestForegroundPermissionsAsync,
 } from "expo-location";
 import React, { useEffect, useRef, useState } from "react";
-import { Alert, Platform, SafeAreaView, Text, View } from "react-native";
+import {
+  Alert,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
 import { Container, HalfContainer } from "./styles";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 
 import { GOOGLE_MAPS_API_KEY } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+interface Coordinate {
+  latitude: number;
+  longitude: number;
+}
 
 export function Home() {
   const [origin, setOrigin] = useState({
@@ -21,6 +34,8 @@ export function Home() {
     latitude: -15.98928,
     longitude: -48.04454,
   });
+
+  const [coordinates, setCoordinates] = useState([]);
 
   const mapRef = useRef(null);
 
@@ -63,12 +78,44 @@ export function Home() {
     });
   }, [origin, destination]);
 
+  function updateLocation(location) {
+    setOrigin({
+      latitude: location.latitude,
+      longitude: location.longitude,
+    });
+  }
+
+  function filterDirections(cs: Coordinate[]) {
+    const directions = [];
+
+    cs.forEach((c) => {
+      if (directions.length > 0) {
+        const distance = Math.sqrt(
+          Math.pow(directions[directions.length - 1].latitude - c.latitude, 2) +
+            Math.pow(
+              directions[directions.length - 1].longitude - c.longitude,
+              2
+            )
+        );
+
+        if (distance > 0.015) {
+          directions.push(c);
+        }
+      } else {
+        directions.push(c);
+      }
+    });
+
+    setCoordinates(directions);
+  }
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
       }}
     >
+      <StatusBar backgroundColor="#000000" barStyle="dark-content" />
       <Container>
         <HalfContainer>
           {origin.latitude !== 0 && origin.longitude !== 0 && (
@@ -77,7 +124,6 @@ export function Home() {
                 flex: 1,
               }}
               ref={mapRef}
-              mapType="mutedStandard"
               initialRegion={{
                 latitude: origin.latitude,
                 longitude: origin.longitude,
@@ -85,6 +131,7 @@ export function Home() {
                 longitudeDelta: 0.005,
               }}
               provider={PROVIDER_GOOGLE}
+              onPress={(event) => updateLocation(event.nativeEvent.coordinate)}
             >
               <MapViewDirections
                 origin={{
@@ -98,6 +145,17 @@ export function Home() {
                 apikey={GOOGLE_MAPS_API_KEY}
                 strokeWidth={3}
                 strokeColor="#8257E5"
+                onStart={(params) => {
+                  console.log(
+                    `Started routing between "${params.origin}" and "${params.destination}"`
+                  );
+                }}
+                onReady={(result) => {
+                  console.log(`Distance: ${result.distance} km`);
+                  console.log(`Duration: ${result.duration} min.`);
+
+                  filterDirections(result.coordinates);
+                }}
               />
               <Marker
                 coordinate={{
@@ -116,10 +174,39 @@ export function Home() {
                 title="UnB Campus Gama"
                 identifier="destination"
               />
+              {coordinates.map((coordinate, index) => (
+                <Marker key={`coordinate_${index}`} coordinate={coordinate} />
+              ))}
             </MapView>
           )}
         </HalfContainer>
-        <HalfContainer></HalfContainer>
+        <HalfContainer>
+          <GooglePlacesAutocomplete
+            placeholder="De onde você quer sair?"
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+            styles={{
+              container: {
+                flex: 0,
+              },
+              textInput: {
+                fontSize: 18,
+              },
+            }}
+            query={{
+              key: GOOGLE_MAPS_API_KEY,
+              language: "pt-br",
+            }}
+            enablePoweredByContainer={false}
+            onPress={(data, details = null) => {
+              setOrigin({
+                latitude: details.geometry.location.lat,
+                longitude: details.geometry.location.lng,
+              });
+            }}
+            fetchDetails={true}
+          />
+        </HalfContainer>
       </Container>
     </SafeAreaView>
   );
